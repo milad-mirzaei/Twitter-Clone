ddf30fb67c0783be638a90b6a7161080dfc6aa8f
MilAd MirzAei <miladmirzaei6802@gmail.com>
2023-05-04 18:37:22 +0430

    commit2

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -12,6 +12,10 @@ const FollowBar = () => {
   }
   const router=useRouter();
 
+  const onclick=(userId:string)=>{
+    router.push(`/users/${userId}`)
+  }
+
 
   return (
     <div className="px-6 py-4 hidden lg:block">
@@ -19,7 +23,7 @@ const FollowBar = () => {
         <h2 className="text-white text-xl font-semibold">Who to follow</h2>
         <div className="flex flex-col gap-1 mt-4">
           {users.map((user: Record<string, any>) => (
-            <div key={user.id} onClick={()=>{router.push(`/users/${user.id}`)}} className="flex flex-row py-2 px-2 rounded-2xl hover:bg-gray-50 hover:cursor-pointer gap-4 hover:gap-5 transition-all">
+            <div key={user.id} onClick={()=>onclick(user.id)} className="flex flex-row py-2 px-2 rounded-2xl hover:bg-gray-50 hover:cursor-pointer gap-4 hover:gap-5 transition-all">
               <Avatar userId={user.id} />
               <div className="flex flex-col">
                 <p className="text-neutral-700 font-semibold hover:cursor-pointer hover:underline text-sm">{user.name}</p>
